62bd6acd7e8f906a29446ed085a8f502472dd652
Taylanne <taylannecastelobrancocavalcant@gmail.com>
2026-04-16 21:55:20 -0300

    subindo

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
-//importação das bibliotecas
+// importação das bibliotecas
 const express = require("express");
+const cors = require("cors");
 
-//iniciação do servidor 
+// iniciação do servidor 
 const app = express();
 const PORT = 3000;
 
+// middlewares
+app.use(cors());
 app.use(express.json());
 
-//verifica de teste para ver se está ativo
+// verificação de teste para ver se está ativo
 app.get('/', (req, res) => {
-res.json({ mensagem: 'Servidor funcionando!' });
+  res.json({ mensagem: 'Servidor funcionando!' });
 });
 
-//importação das rotas utilizadas e suas funções 
+// importação das rotas utilizadas e suas funções 
 app.use("/livros", require("./src/rotas/livros"));
 app.use("/usuarios", require("./src/rotas/usuarios"));
 app.use("/emprestimos", require("./src/rotas/emprestimo"));
 
-//iniciação do servidor na porta 3000
-app.listen(3000, () => {
-  console.log('Rodando na porta 3000');
+// iniciação do servidor na porta 3000
+app.listen(PORT, () => {
+  console.log(`Rodando na porta ${PORT}`);
 });
